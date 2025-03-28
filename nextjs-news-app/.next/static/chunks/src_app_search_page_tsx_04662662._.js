963760794575b8c905a56154757caf5040c97a3e
Rakandiya Shafwan <60130652+Rakandiya@users.noreply.github.com>
2025-03-28 13:28:33 +0700

    Add nextjs-news-app/.next/static/chunks/src_app_search_page_tsx_04662662._.js 1743143311513

---
+++ nextjs-news-app/.next/static/chunks/src_app_search_page_tsx_04662662._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_search_page_tsx_04662662._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_search_page_tsx_04662662._.js",
+  "chunks": [
+    "static/chunks/node_modules_8c29bf8d._.js",
+    "static/chunks/src_c248dcf8._.js"
+  ],
+  "source": "dynamic"
+});
